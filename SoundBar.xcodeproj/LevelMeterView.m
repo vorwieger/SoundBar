633c6f98576594c3847f4b5bfc4bcfd1aabf626d
Peter Vorwieger <peter.vorwieger@me.com>
2011-02-25 23:04:33 +0100

    Star Icon verschoben 

---
+++ SoundBar.xcodeproj/LevelMeterView.m
@@ -0,0 +1,37 @@
+//
+//  LevelMeterView.m
+//  SoundBar
+//
+//  Created by Peter Vorwieger on 25.02.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LevelMeterView.h"
+
+
+@implementation LevelMeterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
